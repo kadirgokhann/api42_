129c12e1ef2d04b9bf6f2ba884e0f294df12b487
Kadir Gökhan Sezer <kadirgokhann@gmail.com>
2023-02-18 10:51:02 +0300

    request

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from src.api42 import API42
 from src.api42.all_moduls import *
 
 api = API42(49)
-print(api.get_accreditations(id=278)[0].cursus_id)
+print(api.get_accreditations()[0].cursus_id)
+response = api.request("get", "/v2/users/49")
