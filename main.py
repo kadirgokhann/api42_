e48e5751e1dcb496e2b30454a990a982a6d3c6ea
Kadir Gökhan Sezer <kadirgokhann@gmail.com>
2023-02-18 10:51:24 +0300

    params

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src.api42.all_moduls import *
 
 api = API42(49)
 print(api.get_accreditations()[0].cursus_id)
-response = api.request("get", "/v2/users/49")
+response = api.request("get", "/v2/users/49", params={"filter[login]": "dummy"})
